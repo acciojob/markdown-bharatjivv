dcc8c4864b162927b42ce4c7f6a89975f92e47cd
BharatJivvTate <hibharatj@gmail.com>
2025-08-22 11:15:18 +0530

    fix - text

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 
 function MarkdownEditor() {
-  const [markdown, setMarkdown] = useState("Hello world # Heading");
+  const [markdown, setMarkdown] = useState("Hello world# Heading");
   const [preview, setPreview] = useState("");
 
   useEffect(() => {
